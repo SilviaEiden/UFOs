2c5cf6f7206d5dd1068ffb6a5335d9bfb888c1ee
Silvia Eiden <im.georgina@gmail.com>
2022-02-25 14:20:40 -0500

    updated file

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,9 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 // Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
+//function printHello() {
+    //console.log("Hello there!");
+//}
 
 //First, create a new function with code to clear existing data tbody.html("");
 function buildTable(data) {
@@ -27,3 +27,28 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
